2a61dc23140e8a7e5ce4826ee67366ad248403a4
DanielReyesZajara <danreyzaj@alum.us.es>
2026-09-16 17:19:27 +0200

    fix: Titulos de notepad sin espacio inicial

--- app/modules/notepad/forms.py
+++ app/modules/notepad/forms.py
@@ -3,6 +3,6 @@ from wtforms import StringField, TextAreaField, SubmitField
 from wtforms.validators import DataRequired, Length
 
 class NotepadForm(FlaskForm):
-    title = StringField('Title', validators=[DataRequired(), Length(max=256)])
+    title = StringField('Title',filters=[lambda value: value.strip() if value else value], validators=[DataRequired(), Length(max=256)])
     body = TextAreaField('Body', validators=[DataRequired()])
     submit = SubmitField('Save notepad')
